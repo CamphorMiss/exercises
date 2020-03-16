75ac609015301e25fcb7af41ac65d4c625368c17
CamphorMiss <1963083540@qq.com>
2020-03-16 21:05:23 +0800

    加油

---
+++ Untitled1.cpp
@@ -0,0 +1,21 @@
+// write your code here cpp
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main()
+{
+    string str,name;
+    while(getline(cin,str))
+    {
+        getline(cin,name);
+        if(name==NULL||str==NULL) break;//·Ç·¨ÅÅ³ý
+        vector<string>v;
+        for(int i=0;i<str.length();i++)
+        {
+            
+        }
+        
+    }
+    return 0;
+}
