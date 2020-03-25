a0408a154aefec2979c7c2c8e5c49ddddde2a2e5
CamphorMiss <1963083540@qq.com>
2020-03-25 22:15:40 +0800

    链表

---
+++ 链表的复制.cpp
@@ -0,0 +1,31 @@
+RandomListNode* Clone(RandomListNode* pHead)
+    {
+        if(!pHead) return NULL;
+        RandomListNode *currNode = pHead;
+        while(currNode){
+            RandomListNode *node = new RandomListNode(currNode->label);
+            node->next = currNode->next;
+            currNode->next = node;
+            currNode = node->next;
+        }
+        currNode = pHead;
+        while(currNode){
+            RandomListNode *node = currNode->next;
+            if(currNode->random){               
+                node->random = currNode->random->next;
+            }
+            currNode = node->next;
+        }
+        //²ð·Ö
+        RandomListNode *pCloneHead = pHead->next;
+        RandomListNode *tmp;
+        currNode = pHead;
+        while(currNode->next){
+            tmp = currNode->next;
+            currNode->next =tmp->next;
+            currNode = tmp;
+        }
+        return pCloneHead;
+    }
+};
+
