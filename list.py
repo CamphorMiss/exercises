152d30e45258e74b95bc827f3c27b7364ad9117f
CamphorMiss <1963083540@qq.com>
2020-04-28 22:32:52 +0800

    pyhton练习

---
+++ list.py
@@ -0,0 +1,22 @@
+#arr=[1,2,3,4]
+#print(arr)
+#print(len(arr))
+#t=(1,2)
+#t[0]=2
+'''age=input("please enter your age:")
+age=int(age)
+if age<2000:
+    print("00前")
+else:
+    print("00后")'''
+'''arr=[1,2,3,4,5,6]
+for a in arr:
+    print(a)'''
+'''sum=0
+for a in range(101):
+    sum+=a
+print(sum)'''
+def fun():
+    pass
+fun()
+
